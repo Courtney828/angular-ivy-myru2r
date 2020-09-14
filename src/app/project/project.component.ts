69697389d85fc347ee9b5e6393da85b7674f2f87
Courtney828 <courtneyserongwa@gmail.com>
2020-09-14 11:34:06 -0700

    2 files modified

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -10,26 +10,19 @@ import { Component, OnInit} from '@angular/core';
 export class ProjectComponent implements OnInit {
 
 
-
-employees = [{name:"Julius", surname:"Mlambo", position:"CEO", age:27},
-                   {name:"Peter", surname:"Brown", position:"Executive Director", age:32},
-                   {name:"Angel", surname:"Zulu", position:"Managing Director", age:22},
-                   {name:"Stanly", surname:"Scott", position:"Chief Financial Office", age:28},
-                   {name:"Amanda", surname:"Solo", position:"Online Manager", age:29},
-                   {name:"Chris", surname:"Malana", position:"Chief Technology officer", age:33},
-                   {name:"Noma", surname:"Mzobe", position:"Developer", age:22},
-                   {name:"Arthur", surname:"Mazibuko", position:"Front-end developer", age:23},
-                   {name:"Anathi", surname:"Xulu", position:"Software Developer", age:21},
-                   {name:"Amo", surname:"Zwane", position:"Intern", age:19},
-  ];
-
-
   constructor() {}
   
 
- 
-
   ngOnInit() {
   }
+ project = [
+    {projectName:"Tracking app",projectDetails:"tracking daily activities", Department:"developer"},
+    {projectName:"Map app",projectDetails:"locating the direction", Department:"Engineering"},
+    {projectName:"Mining app",projectDetails:"stores information of miners", Department:"project manager"},
+    {projectName:"pay roll",projectDetails:"keeps records of payments", Department:"Engineering"},
+    {projectName:"advertising",projectDetails:"promote products and sell", Department:"marketing"},
+    {projectName:"Web application",projectDetails:"creating applications", Department:"web developers"},
+    {projectName:"advertising",projectDetails:"flyers", Department:"Priting"}
+  ]
 
 }
