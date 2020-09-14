a8b1db52b07a31835351c4528e8c246117bde631
Courtney828 <courtneyserongwa@gmail.com>
2020-09-14 08:01:18 -0700

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, VERSION } from '@angular/core';
-
+import{NgForm}from '@angular/forms';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,20 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
-
-
+import{ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
-
 import { MatDialogModule } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
-
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { Routes, RouterModule }from '@angular/router';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
-
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { DialogtwoComponent } from './dialogtwo/dialogtwo.component';
 import { EmployeeComponent } from './employee/employee.component';
@@ -25,7 +22,7 @@ import { DepartmentComponent } from './department/department.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,MatCardModule, BrowserAnimationsModule, MatDialogModule, MatToolbarModule,
-  AppRoutingModule, MatSidenavModule, MatIconModule, MatFormFieldModule
+  AppRoutingModule, MatSidenavModule, MatIconModule, MatFormFieldModule,ReactiveFormsModule
   ],
   declarations: [AppComponent ,HelloComponent, DialogComponent,  DialogtwoComponent, EmployeeComponent, DepartmentComponent ],
   entryComponents:[DialogComponent],
